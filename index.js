e6f24a1a2b6e753f201accb43e9c8fbb5116a361
webinsignia <webdevinsignia@gmail.com>
2021-06-24 16:37:07 +0700

    update

--- index.js
+++ index.js
@@ -7,20 +7,24 @@ const port = 8000;
 app.use(bodyParser.json());
 
 app.post("/seruni", async (req, res) => {
+  axios.defaults.headers.common["Authorization"] = "";
   try {
     if (req.body.token) {
-        axios.defaults.headers.common['Authorization'] = `Bearer ${req.body.token}`;
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${req.body.token}`;
     }
-    if (req.body.method == "post"){
-        const response = await axios.post(req.body.url, req.body.request);
-        res.send(response.data)
+    console.log(axios.defaults.headers.common["Authorization"]);
+    if (req.body.method == "post") {
+      const response = await axios.post(req.body.url, req.body.request);
+      res.send(response.data);
     } else {
-        const response = await axios.get(req.body.url);
-        res.send(response.data)
+      const response = await axios.get(req.body.url);
+      res.send(response.data);
     }
   } catch (error) {
-      console.log(error)
+    console.log(error);
   }
 });
 
-app.listen(port, '0.0.0.0');
+app.listen(port, "0.0.0.0");
